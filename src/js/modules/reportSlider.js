26b15b1909c24a3e8300a8c9aea9f7cc0f3bf32d
Anastasiya <anastasiya.koshelevskaya@nova-test.ru>
2020-10-02 13:59:42 +0300

    adavtive in progress

--- src/js/modules/reportSlider.js
+++ src/js/modules/reportSlider.js
@@ -4,12 +4,18 @@ export default {
   init() {
     Swiper.use([Navigation, Pagination]);
     const reportSlider = new Swiper('.js-slider', {
-      slidesPerView: 3,
-      centeredSlides: true,
+      slidesPerView: 1,
+      centeredSlides: false,
       spaceBetween: 30,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          centeredSlides: true,
+        },
       },
     });
 
