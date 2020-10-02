2986df1deac4134aab2fb68d91eaf362f7f12a4b
Anastasiya <anastasiya.koshelevskaya@nova-test.ru>
2020-10-02 15:39:31 +0300

    mobile in progress

--- src/js/modules/gallerySlider.js
+++ src/js/modules/gallerySlider.js
@@ -4,15 +4,6 @@ import dispatcher from './dispatcher';
 export default {
   init() {
     Swiper.use([Navigation, Pagination]);
-    const galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 30,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-      slideClass: 'gallery-slider__item-thumb',
-      wrapperClass: 'gallery-slider__wrapper',
-    });
     const galleryTop = new Swiper('.gallery-top', {
       spaceBetween: 10,
       slideClass: 'gallery-slider__item-bg',
@@ -22,25 +13,17 @@ export default {
         nextEl: '.gallery-slider__next',
         prevEl: '.gallery-slider__prev',
       },
-      thumbs: {
-        swiper: galleryThumbs,
-      },
     });
-
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
 
     dispatcher.subscribe(({ type }) => {
       if (type === 'document:loaded') {
         setTimeout(() => {
           galleryTop.update();
-          galleryThumbs.update();
         }, 200);
       }
       if (type === 'resize:width') {
         setTimeout(() => {
           galleryTop.update();
-          galleryThumbs.update();
         }, 200);
       }
     });
